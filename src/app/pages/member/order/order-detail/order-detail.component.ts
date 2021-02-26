0d58fdb9b939291341549f2815918f32e784ad28
Bifeldy <bias.astho@gmail.com>
2021-02-26 18:18:57 +0900

    ✔ Order Detail

--- src/app/pages/member/order/order-detail/order-detail.component.ts
+++ src/app/pages/member/order/order-detail/order-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ApiService } from '../../../../shared/services';
 
 @Component({
   selector: 'app-order-detail',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailComponent implements OnInit {
 
-  constructor() { }
+  id = 0;
+  detail = null;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private loader: NgxUiLoaderService,
+    private api: ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params.id;
+    this.getDetail();
+  }
+
+  back() {
+    this.router.navigate(['member/order']);
+  }
+
+  getDetail(): void {
+    this.loader.start();
+    this.api
+      .getData(
+        `admin/transactions/${this.id}`,
+      )
+      .subscribe(
+        (res) => {
+          console.log('res', res);
+          this.detail = res.response;
+          this.loader.stop();
+        },
+        (err) => {
+          this.loader.stop();
+          console.log('err', err);
+        }
+      );
   }
 
 }
